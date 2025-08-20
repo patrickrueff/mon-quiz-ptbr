b012e9e4aba801a77c82fcc8d1e02e9cf3944bb7
Patrick <patrickloma.rueff@gmail.com>
2025-08-20 07:44:48 +0200

    Mises à jour : contenu + quiz/dictée + styles

---
+++ 00000_script_2.bat
@@ -0,0 +1,6 @@
+cd "C:\Users\ICG332\OneDrive - SUEZ\Documents\WORK_PERSO\BRESILIEN"
+
+git status                         # (optionnel) voir ce qui a changé
+git add .                          # ajouter tous les fichiers modifiés/ajoutés/supprimés
+git commit -m "Mises à jour : contenu + quiz/dictée + styles"
+git push                           # envoie vers origin/main
